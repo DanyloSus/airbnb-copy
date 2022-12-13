145d855b9f57ce18207bf33b919a1f9c8cb3e7b8
DanyloSus <danil.sushko19@gmail.com>
2022-12-13 17:08:18 +0200

    Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import Body from "./components/Body";
 
 export default function App() {
     return (
         <div>
             <Header />
+            <Body />
         </div>
     );
 }
---
+++ src/components/Body.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Start from "./Start";
+
+export default function Body () {
+    return (
+        <div>
+            <Start />
+        </div>
+    );
+}
