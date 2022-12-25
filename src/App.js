dd2fe9c70f6328844f135b2aa4735f862bc421e2
DanyloSus <danylo.sushko19@gmail.com>
2022-12-25 18:39:05 +0200

    made my version of props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
-import Header from "./components/Header";
-import Start from "./components/Start";
-import List from "./components/List";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Card from "./components/Card";
 
 export default function App() {
     return (
         <div>
-            <Header />
-            <Start />
-            <List />
+            <Navbar />
+            <Hero />
+            <div id="cards">
+                <Card 
+                    img="1.png"
+                    rating="5.0"
+                    reviewCount="6"
+                    country="USA"
+                    title="Life Lessons with Katie Zaferes"
+                    price="136"
+                />
+                <Card 
+                    img="2.png"
+                    rating="5.0"
+                    reviewCount="6"
+                    country="USA"
+                    title="Life Lessons with Katie Zaferes"
+                    price="136"
+                />
+            </div>
         </div>
     );
 }
